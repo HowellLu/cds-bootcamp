4d4bbec633e6a41ffda925612a7ca2c59069bad3
Howell <hl4631@nyu.edu>
2022-10-05 02:38:17 -0400

    Committhis

--- homework/nlp/bootcamp/_config.py
+++ homework/nlp/bootcamp/_config.py
@@ -2,7 +2,7 @@ import dataclasses
 
 @dataclasses.dataclass
 class BertFineTuningConfig:
-    precision: int = 32
+    precision: int = 16
     max_epochs: int = 20
-    batch_size: int = 8
-    gpus: int = 1
+    batch_size: int = 16
+    gpus: int = 2
